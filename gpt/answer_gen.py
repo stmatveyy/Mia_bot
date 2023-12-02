849d65d3008b98e8ee224e559621009f7180b784
matvey <1132226162@pfur.ru>
2023-12-02 19:53:14 +0300

    fix: slightly better GPT support

--- gpt/answer_gen.py
+++ gpt/answer_gen.py
@@ -4,12 +4,14 @@ g4f.debug.logging = True  # Enable logging
 g4f.check_version = False
 
 _providers = [
-    g4f.Provider.Aichat,
     g4f.Provider.ChatBase,
     g4f.Provider.Bing,
     g4f.Provider.GptGo,
-    g4f.Provider.You,
     g4f.Provider.Yqcloud,
+    g4f.Provider.OpenaiChat,
+    g4f.Provider.ChatgptAi,
+    g4f.Provider.FreeGpt,
+    g4f.Provider.Hashnode
 ]
 
 
@@ -20,14 +22,22 @@ async def run_provider(provider: g4f.Provider.BaseProvider, prompt: str):
             messages=[{"role": "user", "content": prompt}],
             provider=provider,
         )
-        print(response)
+        return response if response else None
 
     except Exception as e:
-        return e
+        print(e)
+        return None
 
 
 async def ask_gpt(prompt: str):
-    calls = [
-        run_provider(provider, prompt) for provider in _providers
-    ]
-    await asyncio.gather(*calls)
+    for provider in _providers:
+        task = asyncio.create_task(run_provider(provider,prompt))
+        while not task.done():
+            await asyncio.sleep(0.1)
+        if task.done():
+            res = task.result()
+            if res:
+                return res
+            else:
+                continue
+    return "Упс! Что-то не так.. Попробуй спросить меня еще раз, или измени текст запроса."
