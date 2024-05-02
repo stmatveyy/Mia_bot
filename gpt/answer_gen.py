32e8c719a878d7136c2dc9f00ad709645b1aa0d3
matvey <1132226162@pfur.ru>
2024-05-02 16:45:37 +0300

    FEAT: GPT mode working again, unstable

--- gpt/answer_gen.py
+++ gpt/answer_gen.py
@@ -1,45 +1,11 @@
 import g4f
-import asyncio
-g4f.debug.logging = True  # Enable logging
-g4f.check_version = False
+g4f.debug.logging = True 
+g4f.check_version = False 
 
-_providers = [
-    g4f.Provider.ChatBase,
-    g4f.Provider.Bing,
-    g4f.Provider.GptGo,
-    g4f.Provider.Yqcloud,
-    g4f.Provider.OpenaiChat,
-    g4f.Provider.ChatgptAi,
-    g4f.Provider.FreeGpt,
-    g4f.Provider.Hashnode
-]
+def ask_gpt(query: str):
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": query}],
+        stream=False)
+    return response
 
-
-async def run_provider(provider: g4f.Provider.BaseProvider,
-                       prompt: str) -> None | str:
-
-    try:
-        response = await g4f.ChatCompletion.create_async(
-            model=g4f.models.default,
-            messages=[{"role": "user", "content": prompt}],
-            provider=provider,
-        )
-        return response if response else None
-
-    except Exception as e:
-        print(e)
-        return None
-
-
-async def ask_gpt(prompt: str):
-    for provider in _providers:
-        task = asyncio.create_task(run_provider(provider, prompt))
-        while not task.done():
-            await asyncio.sleep(0.1)
-        if task.done():
-            res = task.result()
-            if res:
-                return res
-            else:
-                continue
-    return "Упс! Что-то не так.. Попробуй спросить меня еще раз, или измени текст запроса."
--- handlers/GPT.py
+++ handlers/GPT.py
@@ -6,7 +6,7 @@ from aiogram.enums.chat_action import ChatAction
 import FSM.classes
 import keyboards.reply
 import keyboards.inline
-
+from asyncio import sleep
 from gpt.answer_gen import ask_gpt
 
 gpt_router = Router(name='gpt_router')
@@ -28,7 +28,9 @@ async def gpt_turn_off(message: types.Message, state: FSMContext) -> None:
 
 @gpt_router.message(StateFilter(FSM.classes.FSMgpt_states.gpt_mode_on))
 async def gpt_talk(message: types.Message, bot: Bot) -> None:
+
+    m = await bot.send_message(text='Ищу ответ...',chat_id=message.from_user.id)
     await bot.send_chat_action(chat_id=message.chat.id,
                                action=ChatAction.TYPING)
-    gpt_answer = await ask_gpt(message.text)
-    await message.answer(text=gpt_answer)
+    gpt_answer = ask_gpt(message.text)
+    await m.edit_text(text=gpt_answer)
